5c4308fbea7b5da13a0d815ea4159dbcb53f05d0
asantesylvesterkwame <intelligentson17@gmail.com>
2022-11-28 15:31:16 +0000

    cleared everything

--- script.js
+++
@@ -1,7 +0,0 @@
-/*var colors = ['#ff0000', '#00ff00', '#0000ff'];
-var random_color = colors[Math.floor(Math.random() * colors.length)];
-document.querySelector('head').style.color = random_color;*/
-let sketchCells = document.getElementById('head');
-document.addEventListener('click', function() {
-    sketchCells.style.cssText = 'background: black;';
-});
