0c8e1dbf47ae199c91d4296114ae01b9711f49ef
asantesylvesterkwame <intelligentson17@gmail.com>
2022-11-30 11:17:15 +0000

    commented lines of code

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let body = document.body;
 
+let gridBoxTitle = document.createElement('h3');
+body.append(gridBoxTitle);
+gridBoxTitle.innerHTML = 'SKETCH YOUR PATH TO DOOM!';
+gridBoxTitle.style.cssText = 'color: red; font-size: 48px; text-align: center;';
+
+let gridBoxContainer = document.createElement('div');
+body.append(gridBoxContainer);
+gridBoxContainer.style.cssText = 'display: grid; margin: 0  auto; border: 1px solid black; width: 500px; height: 500px;';
+
+let gridBoxItem = document.createElement('div');
+gridBoxContainer.appendChild(gridBoxItem);
+gridBoxItem.style.border = ' 1px solid black';
+
+
 let colorButtonDiv = document.createElement('div');
 body.append(colorButtonDiv);
 
@@ -38,24 +52,8 @@ clearButtonContainer.appendChild(clearButton);
 clearButton.style.cssText = 'box-shadow: 6px 4px 2px; width: 150px; height: 50px; font-size: 24px; font-family: arial;'
 clearButton.innerHTML = 'CLEAR';
 
-let gridBoxTitle = document.createElement('h3');
-body.append(gridBoxTitle);
-gridBoxTitle.innerHTML = 'SKETCH YOUR PATH TO HELL!';
-gridBoxTitle.style.cssText = 'color: red; font-size: 48px; text-align: center;';
 
-let gridBoxContainer = document.createElement('div');
-body.append(gridBoxContainer);
-gridBoxContainer.style.cssText = 'display: grid; margin: 0  auto; border: 1px solid black; width: 500px; height: 500px;';
-
-let gridBoxItem = document.createElement('div');
-gridBoxContainer.appendChild(gridBoxItem);
-gridBoxItem.style.border = ' 1px solid black';
-
-let gridAdjustTitle = document.createElement('h3');
-body.append(gridAdjustTitle);
-gridAdjustTitle.innerHTML = 'ADJUST NUMBER OF BOXES';
-gridAdjustTitle.style.cssText = 'text-align: center; font-size: 24px;';
-
+/*
 let gridAdjusterDiv = document.createElement('div');
 body.append(gridAdjusterDiv);
 gridAdjusterDiv.style.cssText = 'display: flex; justify-content: center;'
@@ -63,22 +61,33 @@ gridAdjusterDiv.style.cssText = 'display: flex; justify-content: center;'
 let gridAdjusterButtonDiv = document.createElement('button');
 gridAdjusterDiv.appendChild(gridAdjusterButtonDiv);
 gridAdjusterButtonDiv.style.cssText = 'font-size: 36px; border-radius: 20px; width: 150px; height: auto;'
-gridAdjusterButtonDiv.innerHTML = '1x1';
+let gridFirstDimensionNumber = 1;
+console.log(gridFirstDimensionNumber);
+let gridSecondDimensionNumber = 1;
+let gridDimensions = gridAdjusterButtonDiv.innerHTML = gridFirstDimensionNumber +'X'+ gridSecondDimensionNumber;
 
 
 let gridAdjuster = document.createElement('input');
 gridAdjusterButtonDiv.appendChild(gridAdjuster);
 gridAdjuster.setAttribute('type', 'range');
+gridAdjuster.setAttribute('min','0');
+gridAdjuster.setAttribute('max', '20');
+gridAdjuster.setAttribute('step', '1');
 gridAdjuster.setAttribute('value', 0);
-let gridAdjusterValue = 0
+let gridAdjusterValue = gridAdjuster.getAttribute('value')
+
 
 gridAdjuster.addEventListener('input', function () {
-    gridAdjusterValue += 1;
-    if (gridAdjusterValue) {
-        
+    if (gridAdjusterValue > 0) {
+        gridFirstDimensionNumber++;
+        gridSecondDimensionNumber++;
+
     }
-    console.log(gridAdjusterValue)
-})
+    
+    
+})*/
+
+
 
 
 
